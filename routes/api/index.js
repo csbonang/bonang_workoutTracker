b0d13e70b4e7532ba29ff621997ce2cad6dd7c7c
csbonang <csgbonang@gmail.com>
2021-08-12 16:05:56 -0700

    updated api/index.js to get and display the total duration on the page

--- routes/api/index.js
+++ routes/api/index.js
@@ -21,6 +21,25 @@ app.get("/api/workouts", (req, res) => {
     });
   });
 
+  // using aggregate 
+  // todo: DO THE SAME FOR THE OTHER GET ROUTES 
+  // aggregate:: uses 1 addField to find the total duration
+app.get("/temp/api", (req, res) => {
+    db.aggregate([{
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" }
+      }
+
+    }], (error, found) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('FOUND',found ); 
+        res.json(found);
+      }
+    });
+  });
+
 // new:true => needed to update the array
 app.put("/api/workouts/:id", (req, res) => {
     db.findByIdAndUpdate(req.params.id,{$push:{exercises:req.body}},{new:true}, (error, found) => {
@@ -47,20 +66,18 @@ app.post("/api/workouts", ({ body }, res) => {
   
   
   app.get("/api/workouts/range", (req, res) => {
-    db.find().limit(15).
+    db.aggregate([{
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" }
+      }}])
+    .limit(15).
     then(records => {
         console.log("Range",records)
         res.json(records)
     }).catch(err=>{
         res.json(err)
     })
-    // , (error, found) => {
-    //   if (error) {
-    //     console.log(error);
-    //   } else {
-    //     res.json(found);
-    //   }
-    // });
+    
   });
 
   // exported router 
